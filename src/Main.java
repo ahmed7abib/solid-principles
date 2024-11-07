311a3f7757b78de2208f78ea3faf2d073e5b8f3b
Ahmed Habib <ahmed.a.habib2020@gmail.com>
2024-11-07 19:50:23 +0200

    Finished All Priciples

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("\n-------- S.R.P -----------\n");
+        System.out.println("\n------------------------- S.R.P ---------------------\n");
         SRPProgram.srpDemo();
 
-        System.out.println("\n-------- O.C.P -----------\n");
+        System.out.println("\n------------------------- O.C.P ---------------------\n");
         OCPProgram.ocpDemo();
 
-        System.out.println("\n-------- L.S.P -----------\n");
+        System.out.println("\n------------------------- L.S.P ---------------------\n");
         LSPProgram.lspViolationDemo();
         LSPProgram.lspBestPracticeWithInheritance();
         LSPProgram.lspBestPracticeWithAbstraction();
 
-        System.out.println("\n-------- I.S.P -----------\n");
+        System.out.println("\n------------------------- I.S.P ---------------------\n");
         ISPProgram.ispDemo();
 
-        System.out.println("\n-------- D.I.P -----------\n");
+        System.out.println("\n------------------------- D.I.P ---------------------\n");
         DIPProgram.dipDemo();
     }
 }
